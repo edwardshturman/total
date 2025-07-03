21000ed431857ec456276e0a49bb89e2a3c98e1b
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2025-07-03 12:37:11 -0700

    Add progress on no user creation on OAuth sign-in

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,8 @@
 import prisma from "@/functions/db"
 import { betterAuth } from "better-auth"
+import { unauthorized } from "next/navigation"
 import { prismaAdapter } from "better-auth/adapters/prisma"
+import { createAuthMiddleware, APIError } from "better-auth/api"
 
 if (!process.env.GOOGLE_CLIENT_ID) {
   throw new Error("Missing env var GOOGLE_CLIENT_ID")
@@ -8,6 +10,9 @@ if (!process.env.GOOGLE_CLIENT_ID) {
 if (!process.env.GOOGLE_CLIENT_SECRET) {
   throw new Error("Missing env var GOOGLE_CLIENT_SECRET")
 }
+
+const whitelistEnvVar = process.env.WHITELIST!
+const whitelist = whitelistEnvVar.split(",")
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -19,5 +24,15 @@ export const auth = betterAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }
+  },
+  hooks: {
+    before: createAuthMiddleware(async (ctx) => {
+      if (ctx.path !== "/sign-in/social") {
+        return
+      }
+      if (ctx.body?.email && !whitelist.includes(ctx.body?.email)) {
+        unauthorized()
+      }
+    })
   }
 })
