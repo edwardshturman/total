c289843ebd28725e6aef2d117feb9873a603c086
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-07-13 14:33:26 -0700

    style: figure out what's the semicolon deal 😤

--- app/plaid/page.tsx
+++ app/plaid/page.tsx
@@ -1,70 +1,79 @@
 // Functions
-import { auth, isAuthorized } from "@/lib/auth"
-import { getItems } from "@/functions/db/items"
-import { redirect, unauthorized } from "next/navigation"
-import { createUser, getUserByEmail } from "@/functions/db/users"
-import { createLinkToken, getAccounts, syncTransactions } from "@/functions/plaid"
-import { convertTransactionForClient, getTransactions } from "@/functions/db/transactions"
+import { auth, isAuthorized } from "@/lib/auth";
+import { getItems } from "@/functions/db/items";
+import { redirect, unauthorized } from "next/navigation";
+import { createUser, getUserByEmail } from "@/functions/db/users";
+import {
+  createLinkToken,
+  getAccounts,
+  syncTransactions,
+} from "@/functions/plaid";
+import {
+  convertTransactionForClient,
+  getTransactions,
+} from "@/functions/db/transactions";
 
 // Components
-import { SignOut } from "@/components/SignOut"
-import { PlaidLink } from "@/components/PlaidLink"
-import { Transactions } from "@/components/Transactions"
+import { SignOut } from "@/components/SignOut";
+import { PlaidLink } from "@/components/PlaidLink";
+import { Transactions } from "@/components/Transactions";
 
 // Types
-import type { Transaction } from "@/generated/prisma"
+import type { Transaction } from "@/generated/prisma";
 
 export default async function Plaid() {
-  const session = await auth()
-  if (!session) redirect("/")
-  const authorized = isAuthorized(session)
-  if (!authorized) unauthorized()
+  const session = await auth();
+  if (!session) redirect("/");
+  const authorized = isAuthorized(session);
+  if (!authorized) unauthorized();
 
-  const linkTokenResponse = await createLinkToken(session!.user!.id!)
+  const linkTokenResponse = await createLinkToken(session!.user!.id!);
 
   // TODO: verification function for having a full user object
-  let user = await getUserByEmail(session!.user!.email!)
+  let user = await getUserByEmail(session!.user!.email!);
   if (!user) {
     user = await createUser({
       name: session!.user!.name!,
-      email: session!.user!.email!
-    })
+      email: session!.user!.email!,
+    });
   }
 
+  // TODO: clean this up, why are we iterating through userItems twice? Also two decrypt calls adds unnecessary overhead
+
   // Sync transactions from all of the user's accounts
-  const userItems = await getItems(user.id)
+  const userItems = await getItems(user.id);
   for (const item of userItems) {
-    const accessToken = item.accessToken
-    await syncTransactions(accessToken)
+    // TODO!!! Here decrypt access token
+    const accessToken = item.accessToken;
+    await syncTransactions(accessToken);
   }
 
   // Aggregate transactions across all of the user's accounts
-  const transactions: Transaction[] = []
+  const transactions: Transaction[] = [];
   for (const item of userItems) {
-    const { accounts } = await getAccounts(item.accessToken)
+    // TODO!!! Decrypt again
+    const { accounts } = await getAccounts(item.accessToken);
     for (const account of accounts) {
-      const accountTransactions = await getTransactions(account.account_id)
-      transactions.push(...accountTransactions)
+      const accountTransactions = await getTransactions(account.account_id);
+      transactions.push(...accountTransactions);
     }
   }
-  const clientFriendlyTransactions = transactions.map(convertTransactionForClient)
+
+  const clientFriendlyTransactions = transactions.map(
+    convertTransactionForClient
+  );
 
   // TODO: After the user connects their accounts, add a new option for "add a new account"
   return (
     <>
       <p>User: {session!.user!.email!}</p>
       <p>Link token: {linkTokenResponse.link_token}</p>
-      {
-        userItems.length === 0
-        ?
-          <PlaidLink
-            linkToken={linkTokenResponse.link_token}
-            userId={user.id}
-          />
-        :
-          <Transactions transactions={clientFriendlyTransactions} />
-      }
+      {userItems.length === 0 ? (
+        <PlaidLink linkToken={linkTokenResponse.link_token} userId={user.id} />
+      ) : (
+        <Transactions transactions={clientFriendlyTransactions} />
+      )}
       <SignOut />
     </>
-  )
+  );
 }
